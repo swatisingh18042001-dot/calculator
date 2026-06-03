701d2d45813b140c6ba5ae699dfa892590c202cd
swatisingh <swatisingh18042001@gmail.com>
2026-06-03 17:00:13 +0530

    modified add.py

--- add.py
+++ add.py
@@ -3,6 +3,10 @@
 a=3
 b=4
 <<<<<<< HEAD
+<<<<<<< HEAD
+print("sum=",a+b)
+>>>>>>> a69fb69 (printing output of add.py)
+=======
 print("sum=",a+b)
 >>>>>>> a69fb69 (printing output of add.py)
 =======
